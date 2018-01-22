699a5d50914246e02f4af0a42283a5973336ab94
Rapha <raphaelmueller1@hotmail.com>
2018-01-22 14:40:44 +0100

    test commit

--- app/src/main/java/com/example/rapha/healthcheck/SummaryActivity.kt
+++ app/src/main/java/com/example/rapha/healthcheck/SummaryActivity.kt
@@ -1,7 +1,10 @@
 package com.example.rapha.healthcheck
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
 
 class SummaryActivity : AppCompatActivity() {
 
